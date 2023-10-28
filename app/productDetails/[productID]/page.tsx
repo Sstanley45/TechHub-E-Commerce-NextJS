4a0be878fa8c97be8fff5b6e239d1c5f89519f05
Sstanley45 <stanleynjuguna445@gmail.com>
2023-10-28 16:45:26 +0300

    product-review

--- app/productDetails/[productID]/page.tsx
+++ app/productDetails/[productID]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { product } from "@/utils/productTemp";
 import ProductDetailsComponent from "../ProductDetailsComponent";
 import Container from "@/app/components/Container";
+import ListRating from "./ListRating";
 
 interface IParams {
   productId?: string;
@@ -13,7 +14,11 @@ const ProductPage = ({ params }: { params: IParams }) => {
   return (
     <div className="p-8">
       <Container>
-        <ProductDetailsComponent product={product} />;
+        <ProductDetailsComponent product={product} />
+        <div className="flex flex-col mt-20 gap-4">
+          <div><ListRating product={ product } /></div>
+          <div>List</div>
+        </div>
       </Container>
     </div>
   );
